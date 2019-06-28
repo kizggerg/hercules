ef0f0acc504e0c209e57932504c39dccbfba2386
kizggerg <gregory_gzik@acl.com>
2019-06-28 12:08:41 -0700

    Integrate Github

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { BrowserWindow } from "electron";
 import Tunnel from "./Services/Tunnel";
 import { windows, loadBrowser, registerOnClose } from "./Electron/Setup";
+import { GetPullRequestComments } from "./Services/GitHub";
 
 let Windows: { [key: string]: BrowserWindow } = windows;
+
+const playground = async () => {
+  let comments = await GetPullRequestComments("acl-services", "paprika", 73);
+  Windows["comments"].webContents.send("comments-data", comments);
+};
 
 const main = async (): Promise<void> => {
   let url = await Tunnel("http://us-org.projects.highbond.local:9396");
@@ -17,4 +23,5 @@ const exit = (error: Error) => {
 
 main()
   .then(() => registerOnClose(process.exit))
+  .then(playground)
   .catch(exit);
